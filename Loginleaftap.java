e71aed3ebe779ac1abaace2d317fad1ca2b6229b
Kavin-Sathya <62929995+Kavin-Sathya@users.noreply.github.com>
2020-04-01 19:02:18 +0530

    Add files via upload
    Day2-CW-Login to Leaftap

---
+++ Loginleaftap.java
@@ -0,0 +1,20 @@
+package week1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Loginleaftap {
+
+	public static void main(String[] args) {
+	
+		System.setProperty("webdriver.chrome.driver",".\\drivers\\Chromedriver\\chromedriver1.exe");
+		
+		ChromeDriver driver=new ChromeDriver();
+		
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.findElementById("username").sendKeys("DemoSalesManager");
+		driver.findElementById("password").sendKeys("crmsfa");
+		driver.findElementByClassName("decorativeSubmit").click();
+
+	}
+
+}
