44e1d08cbe13aa0d5c03010a6a1c1bc13a46b69c
Kavin-Sathya <62929995+Kavin-Sathya@users.noreply.github.com>
2020-04-03 19:46:45 +0530

    Day4-CW-Array2

---
+++ Arrayprog2.java
@@ -0,0 +1,20 @@
+package week1;
+
+public class Arrayprog2 {
+
+	public static void main(String[] args) {
+		
+		int[] a= {200,500,10,3000};
+		
+		
+		for (int i = 0; i < a.length; i++) {
+			
+			if(a[i]==10)
+			{
+				System.out.println("Position of 10 is :"+ i);
+			}
+			
+		}
+	}
+
+}
